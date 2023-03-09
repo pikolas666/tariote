868874f07fbd715b4442d267d87efbafeee0d662
pikolas666 <115988593+pikolas666@users.noreply.github.com>
2023-03-09 17:24:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,21 @@
 //           $(this).removeClass('transition');
 //       });
 //   });
-
+const fs = require('fs'); 
+ 
+var directory = './pictures/gamta'; 
+ 
+fs.readdir(directory, (err, files) => { 
+	if(err) { 
+    console.log('error')
+		// handle error; e.g., folder didn't exist 
+	} else {
+    const list = files;
+    console.log(list)
+    directory.forEach(file => {
+      gallery.innerHTML += `<a href="pictures/gamta/${picture}"><img src="pictures/gamta/${picture}"></a>`
+console.log(gallery)
+    })
+  }
+	// 'files' is an array of the files found in the directory 
+}); 
